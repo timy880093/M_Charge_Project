90f79d3ee7f3be78e338fe57bdf98fdae4bc09ae
eason <eason@4b8841f4-42b5-4263-839e-868dbe4382dd>
2018-04-16 02:19:00 +0000

    在專案中加入freemarker的lib，修正後端寫錯form request type的部份。
    git-svn-id: http://svn.gwis.com.tw:88/repos/M_Charge_Project/trunk@5410 4b8841f4-42b5-4263-839e-868dbe4382dd

--- src/main/java/com/gate/web/servlets/backend/cash/CashSearchServlet.java
+++ src/main/java/com/gate/web/servlets/backend/cash/CashSearchServlet.java
@@ -465,7 +465,7 @@ public class CashSearchServlet extends MvcBaseServlet {
      * @return
      * @throws Exception
      */
-    @RequestMapping(method = RequestMethod.GET, params = "method=emailYM", produces = "application/json;charset=utf-8")
+    @RequestMapping(method = RequestMethod.POST, params = "method=emailYM", produces = "application/json;charset=utf-8")
     public @ResponseBody
     String emailYM(@RequestParam MultiValueMap<String, String> paramMap,
                      @RequestHeader HttpHeaders headers, Model model
